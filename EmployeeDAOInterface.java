6f374dd62bfec5bab02f8a2126e8698f4ead5345
SIDDHARTH VAJPAYEE <91963908+SIDDHARTHVAJPAYEE@users.noreply.github.com>
2021-12-23 09:47:50 +0530

    Add files via upload

---
+++ EmployeeDAOInterface.java
@@ -0,0 +1,18 @@
+package com.thinking.machines.hr.dl.interfaces.dao;
+import java.util.*;
+import com.thinking.machines.hr.dl.exceptions.*;
+import com.thinking.machines.hr.dl.interfaces.dto.*;
+public interface EmployeeDAOInterface
+{
+public void add(EmployeeDTOInterface employeeDTO) throws DAOException;
+public void update(EmployeeDTOInterface employeeDTO) throws DAOException;
+public void delete(String employeeId) throws DAOException;
+public Set<EmployeeDTOInterface> getAll() throws DAOException;
+public Set<EmployeeDTOInterface> getByDesignationCode(int designationCode) throws DAOException;
+public boolean isDesignationAlloted(int designationCode) throws DAOException;
+public EmployeeDTOInterface getByEmployeeId(String employeeId) throws DAOException;
+public EmployeeDTOInterface getByPANNumber(String panNumber) throws DAOException;
+public EmployeeDTOInterface getByAadharCardNumber(String aadharCardNumber) throws DAOException;
+public int getCount() throws DAOException;
+public int getCOuntByDesignation(int designationCode) throws DAOException;
+}
